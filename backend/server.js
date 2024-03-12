48b5e5fbc3293256ee518a611a8011b1a590d384
vishnupriyanarayana <113781108+vishnupriyanarayana@users.noreply.github.com>
2024-03-13 00:22:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const routes = require("./routes");
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://10.0.0.4:27017/todos", {
+  await mongoose.connect("mongodb://40.121.164.186:27017/todos", {
     useUnifiedTopology: true,
     useNewUrlParser: true,
   });
