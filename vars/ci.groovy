1a1c38f604a5750c0bf453c848240f61589f014f
vishnupriyanarayana <113781108+vishnupriyanarayana@users.noreply.github.com>
2024-04-09 12:38:18 +0530

    Create ci.groovy

---
+++ vars/ci.groovy
@@ -0,0 +1,56 @@
+pipeline {
+    agent {
+        kubernetes {
+            yaml '''
+              apiVersion: v1
+              kind: Pod
+              spec:
+                containers:
+                - name: docker 
+                  image: docker:20-dind
+                  securityContext:
+                      privileged: true
+                  tty: true
+                - name: kubectl
+                  image: bitnami/kubectl
+                  tty: true
+              '''          
+
+        }
+
+    }
+    stages {
+      stage ("Git checkout") {
+        steps {
+          script {
+            git "https://github.com/vishnupriyanarayana/docker-frontend-backend-db.git"
+          }
+        }
+      }
+      stage ("Docker build") {
+        steps {
+          container ("docker") {
+            script {
+              sh 'docker build -t frontend .'
+              sh 'docker tag frontend vishnupriya772002/frontend:9'
+              sh 'docker push vishnupriya772002/frontend:9'
+            }
+          }
+          
+        }
+      }
+      stage ("Deploy service") {
+        steps {
+          container ("docker") {
+            script {
+              sh 'kubectl apply -f frontendkube.yaml -n vishnu'
+              sh 'kubectl port-forward svc/frontend-service 3000:3000 -n vishnu'
+            }
+
+          }
+        }
+      }
+    }
+    
+
+}
